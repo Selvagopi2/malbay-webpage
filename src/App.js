6b90a2fc32ea26f786e8751c2d6c27b4f18e8b04
selvagopi <selvagopi2@gmail.com>
2023-10-27 15:09:48 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+
+function Home() {
+  return <div>Home Page</div>;
+}
+
+function About() {
+  return <div>About Page</div>;
+}
+
+function Services() {
+  return <div>Services Page</div>;
+}
+
+function Locations() {
+  return <div>Locations Page</div>;
+}
+
+function Contact() {
+  return <div>Contact Page</div>;
+}
+
+function Fleets() {
+  return <div>Fleets Page</div>;
+}
+
+function Pricing() {
+  return <div>Pricing Page</div>;
+}
+
+function BookNow() {
+  return <div>Book Now Page</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/locations" component={Locations} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/fleets" component={Fleets} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/booknow" component={BookNow} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
