8ec3d53ce411b10338a4245162acdf4ff8358596
selvagopi <selvagopi2@gmail.com>
2023-10-27 15:28:04 +0530

    headerapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
-function Home() {
-  return <div>Home Page</div>;
-}
 
 function About() {
   return <div>About Page</div>;
