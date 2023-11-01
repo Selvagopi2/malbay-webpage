83cbb7cba5f9c058bedffa61968bc7a77f66a219
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2023-11-01 16:32:50 +0530

    footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,62 +2,64 @@ import React from 'react';
 import './footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-// import { faFacebookSquare, faTwitterSquare, faInstagram, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
-
-// import { Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 
 
 const Footer = () => {
   return (
-    <footer className="bg-dark-text-light">
-    
-         <div className="container">
-         <div className="row footer-first-container">
-         <div className="col-12 col-md-4 mb-3 para">
-            <h2  className='logoname'>Malbay Express</h2>
-            <p>Malbay Express offers dependable, secure, and cost-effective MVP Private Car journeys from Singapore to Johor Bahru, Legoland Malaysia, and various other destinations within Malaysia.</p>
-        </div>
+    <footer>
+    <article>
+      <h2>Try Sg2mycab For your Travel.</h2>
+      <button> <p>Contact Us</p> </button>
+    </article>
 
-     <div className="col-12 col-md-4 mb-3">
-             <a href="/Home">
-                <p>HOME</p>
-            </a>
-            <a href="/About">
+    <section class="top">
+      <img src="logo"/> 
+
+      <ul>
+        <li>
+        <h3 className='logoname'>Sg2mycab</h3>
+         <p>At Sg2mycab Express, we provide reliable, safe, and budget-friendly MVP Private Car services for travelers heading from Singapore to Johor Bahru, Legoland Malaysia, and numerous other destinations across Malaysia. Your trusted partner for seamless and cost-effective journeys.</p>
+        </li> 
+        
+     <li>
+         <div className='page'>
+          <a href="/Home">
+               <p>HOME</p>
+             </a>
+         <a href="/About">
                 <p>ABOUT</p>
-            </a>
-            <a href="/Serivces">
+             </a>
+             <a href="/Serivces">
                 <p>SERVICES</p>
-            </a>
-            <a href="/Locations">
-                <p>LOCATIONS</p>
-            </a> 
-            <a href="/Fleets">
-                <p>FLEETS</p>
-            </a>
-            <a href="/Pricing">
-                <p>PRICING</p>
-            </a>
-            <a href="/Contact">
-                <p>CONTACT</p>
-            </a>
-        </div>
+             </a>
+             <a href="/Locations">
+                 <p>LOCATIONS</p>
+             </a> 
+             <a href="/Fleets">
+                 <p>FLEETS</p>
+             </a>
+             <a href="/Pricing">
+                 <p>PRICING</p>
+             </a>
+             <a href="/Contact">
+                 <p>CONTACT</p>
+             </a></div> 
+        </li>
 
-        <div className="sb_footer-links_div">
-             <p><FontAwesomeIcon icon={faPhone} className="me-2" />  +(91)6369487527 </p><br/>
-              <p><FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" /> 22,Teachers colony, <br/> Tirunelveli,<br/> TamilNadu-628810,<br/></p><br/>
-              <p><FontAwesomeIcon icon={faEnvelope } className="me-2"/>  flynewtours@gmail.com<br/></p>
-        </div>
-             </div>
+        <li>
+          <h3>Contact</h3>
+          <p><FontAwesomeIcon icon={faPhone} className="me-2" />+(65)82335292</p>
+          <p><FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />316C Anchorvale Link 05 - 209 <br/>Anchorvale Gardens<br/>Singapore<br/>543316</p>
+          <p><FontAwesomeIcon icon={faEnvelope} className="me-2" />flynewtours@gmail.com</p>
+        </li>
+      </ul>
+    </section>
 
-             <div className="text-center"><br/>
-                <p>@{new Date().getFullYear()} CopyRights&copy.Developed by DigiSailor.</p>
-            </div>
+    <section class="bottom">Copyrights©2023.Developed by DigiSailor </section>
+  </footer>
 
-    </div>
-   
-    </footer>
   );
 }
 
